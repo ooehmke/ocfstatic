ae0be4b4148bbdad96e918e0cba280c2b67c2a55
Tavi Oehmke <ooehmke@ocf.berkeley.edu>
2022-09-30 11:52:58 -0700

    Staff training lab four, task 2

--- src/pages/About/Staff.vue
+++ src/pages/About/Staff.vue
@@ -192,7 +192,14 @@ export default {
         role: ["Online People"],
         face: "https://bulma.io/images/placeholders/1280x960.png",
         icon: "https://bulma.io/images/placeholders/96x96.png"
-      }
+	},
+	[
+	handle: "ooehmke"
+	name: "Octavia Oehmke",
+	role: ["Tavi"],
+        face: "https://bulma.io/images/placeholders/1280x960.png",
+        icon: "https://bulma.io/images/placeholders/96x96.png"
+	},
     ];
   }
 };
